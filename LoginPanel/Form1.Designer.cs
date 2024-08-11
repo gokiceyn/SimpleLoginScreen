981f9aeadf577e31d6d7a9f7b1fcaae57dfb151a
gökçen <gken@WINDOWS-V7FCRNT>
2024-08-11 16:02:56 +0300

    Add project files.

---
+++ LoginPanel/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace LoginPanel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            panel2 = new Panel();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(66, 147);
+            label1.Name = "label1";
+            label1.Size = new Size(32, 21);
+            label1.TabIndex = 0;
+            label1.Text = "TC:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(15, 188);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Password:";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(104, 144);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(168, 29);
+            maskedTextBox1.TabIndex = 2;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(104, 185);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(168, 29);
+            textBox1.TabIndex = 3;
+            textBox1.UseSystemPasswordChar = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LavenderBlush;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(1, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(762, 126);
+            panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.White;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(436, 26);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(104, 74);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe Script", 16F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(11, 43);
+            label3.Name = "label3";
+            label3.Size = new Size(382, 37);
+            label3.TabIndex = 0;
+            label3.Text = " Online Course Entrance Panel";
+            label3.Click += label3_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.LavenderBlush;
+            panel2.Location = new Point(1, 374);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(762, 25);
+            panel2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(189, 220);
+            button1.Name = "button1";
+            button1.Size = new Size(83, 29);
+            button1.TabIndex = 6;
+            button1.Text = "login";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.ForeColor = Color.Black;
+            checkBox1.Location = new Point(278, 189);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(151, 25);
+            checkBox1.TabIndex = 7;
+            checkBox1.Text = "forgot password";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Pink;
+            ClientSize = new Size(762, 399);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(textBox1);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            ForeColor = Color.MediumTurquoise;
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "User Login Panel";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button1;
+        private CheckBox checkBox1;
+        private Label label3;
+        private PictureBox pictureBox1;
+    }
+}
